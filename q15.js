926f604a410ccc32129009e8992ca8782f44ed9f
InsiaMurtaza <165552743+InsiaMurtaza@users.noreply.github.com>
2024-05-03 11:24:01 +0500

    Add files via upload

---
+++ q15.js
@@ -0,0 +1,5 @@
+"use strict";
+let invitationTodinner1 = ["my mother in law", "my sisters and family", "my sisters in law and family"];
+for (let i = 0; i < invitationTodinner1.length; i++) {
+    console.log(`I coordially invite ${invitationTodinner1[i]} for dinner on Sunday 28th April 2024`);
+}
---
+++ q15.ts
@@ -0,0 +1,4 @@
+let invitationTodinner1:string[]=["my mother in law","my sisters and family","my sisters in law and family"];
+for (let i=0;i<invitationTodinner1.length;i++){
+    console.log(`I coordially invite ${invitationTodinner1[i]} for dinner on Sunday 28th April 2024`);
+}
